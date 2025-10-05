0f688040e76dcbd3d58536837fdfe459bc26c559
Horovtom <tom.horovsky@gmail.com>
2025-10-05 12:21:32 +0200

    Upgrading to ue 5.6, adding cursor fx under click

--- Source/Raviolli.Target.cs
+++ Source/Raviolli.Target.cs
@@ -9,7 +9,7 @@ public class RaviolliTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 		ExtraModuleNames.Add("Raviolli");
 	}
 }
